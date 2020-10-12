56e62a1d4bb78dc8c3dfd16dcaa8bb5ee64fe776
Gaurav Singh <gauravkorlasingh@gmail.com>
2020-10-12 15:40:55 +0530

    Topbar scaffolding, Logo component and added the logo

---
+++ gigs-web-app/components/Logo.js
@@ -0,0 +1,12 @@
+import Image from 'react-bootstrap/Image'
+
+function Logo () {
+  return (
+    <Image
+      src="/gigsLogo.png"
+      alt="Gigs Logo"
+      width="140" />
+  )
+}
+
+export default Logo
---
+++ gigs-web-app/components/Topbar.js
@@ -0,0 +1,6 @@
+function Topbar(props) {
+  return (
+    <>
+    </>
+  )
+}
